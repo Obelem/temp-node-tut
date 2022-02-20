c09a0da2e4923e6b88d5fad307153d28a33a3881
Obelem <ibbraidejesse@gmail.com>
2022-02-20 19:34:03 +0100

    first commit

--- app.js
+++ app.js
@@ -12,7 +12,7 @@
 // manual approach (create package.json in the root, create properties, etc)
 // npm init (step by step, press enter to skip)
 // npm init -y (everything default)
-
+// 
 const _ = require("lodash")
 
 const items = [1, [2,[3,[4]]]]

402f214e0ca93eab1e5f55df3ea882e76d435f94
Obelem <ibbraidejesse@gmail.com>
2022-02-20 18:52:21 +0100

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,15 @@
+// GLOBALS - NO WINDOW!!
+
+// __dirname   - path to current directory
+// __filename  - file name
+// require     - function to use modules (CommonJs)
+// module      - info about current module (file)
+// process     - info about env where the program is being executed
+
+const names = require("./4-names");
+const sayHi = require("./5-utils")
+// console.log(names);
+
+const data = require("./6-alternative-flavor")
+require("./7-mind-grenade")
+/*   */
